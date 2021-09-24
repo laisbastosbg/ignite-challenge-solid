84d0707400ebad0d3b0b326513a0b0fc2ceb8e02
Lais Godinho <lais2bg@gmail.com>
2021-09-24 00:45:02 -0300

    use cases

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -7,10 +7,16 @@ interface IRequest {
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const emailIsTaken = this.usersRepository.findByEmail(email);
+
+    if (emailIsTaken) throw new Error("Email is already taken!");
+
+    const user = this.usersRepository.create({ name, email });
+
+    return user;
   }
 }
 
