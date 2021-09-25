e838401e254c52cfea3a1d97e1af78dc103f594b
Lais Godinho <lais2bg@gmail.com>
2021-09-24 22:19:07 -0300

    heroku setup

--- dist/src/server.js
+++ dist/src/server.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var _1 = require(".");
-_1.app.listen(3333, function () { return console.log("Server is running!"); });
+_1.app.listen(process.env.PORT || 3333, function () { return console.log("Server is running!"); });
